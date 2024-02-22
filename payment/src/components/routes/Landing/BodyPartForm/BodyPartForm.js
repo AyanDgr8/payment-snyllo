4a48094e9d03e75c4326873c2c8545f0e9c17bca
Ayan <ayandgr5@gmail.com>
2024-02-22 19:53:43 +0530

    lastly

--- payment/src/components/routes/Landing/BodyPartForm/BodyPartForm.js
+++ payment/src/components/routes/Landing/BodyPartForm/BodyPartForm.js
@@ -1,38 +1,37 @@
 // src/components/routes/LandingPage/BodyPartForm/BodyPartForm.js
 
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './BodyPartForm.css'; 
 
 const availableBodyParts = {
   men: {
-    trial: ['face', 'body', 'legs'],
-    permanent: ['face', 'body', 'legs', 'arms', 'back'],
+    trial: ['chin', 'upperlip', 'underarms'],
+    permanent: ['full', 'face', 'legs', 'arms', 'chest', 'back'],
   },
   women: {
-    trial: ['face', 'body', 'legs'],
-    permanent: ['face', 'body', 'legs', 'arms', 'back'],
+    trial: ['chin', 'upperlip', 'underarms'],
+    permanent: ['full', 'face', 'legs', 'arms', 'chest', 'back'],
   },
   others: {
-    trial: ['face', 'body', 'legs'],
-    permanent: ['face', 'body', 'legs', 'arms', 'back'],
+    trial: ['chin', 'upperlip', 'underarms'],
+    permanent: ['full', 'face', 'legs', 'arms', 'chest', 'back'],
   },
 };
 
 const trialMap = {
-  face: 1000,
-  body: 2000,
-  legs: 1500,
-  arms: 2500,
-  back: 3000,
+  chin: 2000,
+  upperlip: 2000,
+  underarms: 2000,
 };
 
 const permanentMap = {
-  face: 5000,
-  body: 10000,
+  full: 11000,
+  face: 3000,
   legs: 5000,
-  arms: 7500,
-  back: 10000,
+  arms: 6000,
+  chest: 4000,
+  back: 3500,
 };
 
 const BodyPartForm = () => {
@@ -44,61 +43,81 @@ const BodyPartForm = () => {
     purchaseType: 'trial',
     selectedBodyParts: [],
     selectedDate: '',
+    coupon: '',
   });
+  
   const [submitStatus, setSubmitStatus] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!formData.name || !formData.phoneNumber || !formData.email || !formData.selectedDate) {
+    if (!formData.name || !formData.phoneNumber || !formData.email || !formData.selectedDate || formData.selectedBodyParts.length === 0) {
       alert('Please fill in all the required fields');
       return;
     }
 
-    const apiUrl = 'https://snyllo-payment.onrender.com/user-details-bookform';
+    const phoneRegex = /^\d{10}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!phoneRegex.test(formData.phoneNumber)) {
+      alert('Please enter a valid phone number');
+      return;
+    }
+
+    if (!emailRegex.test(formData.email)) {
+      alert('Please enter a valid email address');
+      return;
+    }
+
+    const apiUrl = "https://snyllo-payment.onrender.com/user-details-bookform";
 
     try {
-      const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (!response.ok) {
-        throw new Error('Error submitting form');
-      }
-
+      // Stringify the selectedBodyParts array before sending it in the request
+      const requestData = {
+        ...formData,
+        selectedBodyParts: JSON.stringify(formData.selectedBodyParts),
+      };
+  
+      await axios.post(apiUrl, requestData);
       console.log('Submission successful');
       setSubmitStatus('success');
+      resetForm();
     } catch (error) {
       console.error('Error submitting form:', error);
       alert('Error submitting the form. Please try again.');
       setSubmitStatus('error');
     }
   };
+  
+  const resetForm = () => {
+    setFormData({
+      name:'',
+      phoneNumber:'',
+      email:'',
+      gender:'',
+      purchaseType:'',
+      selectedBodyParts:[],
+      selectedDate:'',
+      coupon:'',
+    });
+  };
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
   
-    // For checkboxes, handle selectedBodyParts separately
     if (type === 'checkbox' && name === 'selectedBodyParts') {
-      // If checked, add the value to the selectedBodyParts array
       if (checked) {
         setFormData((prevData) => ({
           ...prevData,
           selectedBodyParts: [...prevData.selectedBodyParts, value],
         }));
       } else {
-        // If unchecked, remove the value from the selectedBodyParts array
         setFormData((prevData) => ({
           ...prevData,
           selectedBodyParts: prevData.selectedBodyParts.filter(part => part !== value),
         }));
       }
     } else {
-      // For other input fields, update the form data normally
       setFormData((prevData) => ({
         ...prevData,
         [name]: value,
@@ -109,20 +128,35 @@ const BodyPartForm = () => {
   
   const calculateTotalPrice = () => {
     const selectedPriceMap = formData.purchaseType === 'trial' ? trialMap : permanentMap;
-    return formData.selectedBodyParts.reduce((total, part) => total + selectedPriceMap[part], 0);
+    let totalPrice = formData.selectedBodyParts.reduce((total, part) => total + selectedPriceMap[part], 0);
+  
+    if (formData.coupon === 'SNYLLO25') {
+      totalPrice *= 0.75; 
+    } else if (formData.coupon === 'SNYLLO40'){
+      totalPrice *= 0.6;
+    }
+
+    return totalPrice;
   };
 
+  useEffect(() => {
+    if (submitStatus === 'success') {
+      const resetFormTimeout = setTimeout(() => {
+        setSubmitStatus(null);
+        resetForm();
+      }, 3000);
+      return () => clearTimeout(resetFormTimeout);
+    }
+  }, [submitStatus]);
 
   return (
     <div>
       <section className='above-form'>
-
         <img 
           src="/upload/yoyo.jpg"
           className='yoyo'
           alt="yoyo"
         />
-
         <div className='form-header'>
           <img 
           src="/upload/logo.png"
@@ -130,19 +164,12 @@ const BodyPartForm = () => {
           alt="logo"
           />
         </div>
-
       </section>
       <div className='container-payment'>
-        
-      <form 
-      onSubmit={handleSubmit} 
-      className='form-payment'
-      >
-        <div className='form-heading'>
-          <div className='form-line1'>Book Your Appointment</div>
-        </div>
-
-        
+        <form onSubmit={handleSubmit} className='form-payment'>
+          <div className='form-heading'>
+            <div className='form-line1'>Book Your Appointment</div>
+          </div>
           <div className='basic'>
             <label>
               Name
@@ -155,14 +182,9 @@ const BodyPartForm = () => {
               onChange={handleChange} 
               required  
               />
-
             </label>
-
-            
-            
             <label>
               Contact
-
               <input 
               type="text" 
               placeholder="Phone Number*"
@@ -173,14 +195,9 @@ const BodyPartForm = () => {
               pattern="[0-9]{10}"
               required
               />
-
             </label>
-
-            
-            
             <label>
               Email
-
               <input 
               type="email" 
               placeholder="Email Address*"
@@ -190,15 +207,10 @@ const BodyPartForm = () => {
               onChange={handleChange}
               required
               />
-
             </label>
           </div>
-          
-          
-          
-
           <div className='based'>
-              <label>
+            <label>
               Gender
               <select
                   type="text"
@@ -209,17 +221,13 @@ const BodyPartForm = () => {
                   onChange={handleChange}
                   required
               >
-                      
                 <option value="men">Men</option>
                 <option value="women">Women</option>
                 <option value="others">Others</option>
-
               </select>
             </label>
-
             <label>
-              Select Package
-
+              Choose One
             <select
                 type="text"
                 placeholder="Trial"
@@ -231,13 +239,9 @@ const BodyPartForm = () => {
             >
               <option value="trial">Trial</option>
               <option value="permanent">Package</option>
-
             </select>
-
             </label>
-
-
-            <label>
+            <label className='datee'>
               Date
              <input 
               type="date" 
@@ -264,15 +268,22 @@ const BodyPartForm = () => {
               /> {part.charAt(0).toUpperCase() + part.slice(1)} - ₹{formData.purchaseType === 'trial' ? trialMap[part] : permanentMap[part]}
             </label>
           ))}
-
           <br /><br />
-          
-
+          <label className='couponn'>
+            Coupon
+              <input 
+              type="text" 
+              placeholder="Enter Your Coupon"
+              name="coupon"
+              className="coupon"
+              value={formData.coupon} 
+              onChange={handleChange} 
+              />
+          </label>
           <label className='total'>
             Total Amount: ₹{calculateTotalPrice()}
           </label><br /><br />
-
-          <button className='submit-button' type="submit" onClick={handleSubmit}>Proceed to Payment</button>
+          <button className='submit-button' type="submit">Proceed to Payment</button>
         </form>
         {submitStatus && (
           <p className={submitStatus === 'success' ? 'success-message' : 'error-message'}>
@@ -280,9 +291,6 @@ const BodyPartForm = () => {
           </p>
         )}
       </div>
-
-      
-
       <button className='website'>
             <a 
             href="https://snylloestetica.com/"
@@ -295,4 +303,3 @@ const BodyPartForm = () => {
 };
 
 export default BodyPartForm;
-
